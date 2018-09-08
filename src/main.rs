cae95c665cd892d83ce02cda5d1322a4f55edaa6
ryym <ryym.64@gmail.com>
2018-09-08 19:10:36 +0900

    Connect to Postgres

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,36 @@
 extern crate actix_web;
+extern crate diesel;
+extern crate dotenv;
 extern crate env_logger;
 
 use actix_web::{server, App, HttpRequest, Responder};
-use std::env;
+use diesel::prelude::*;
+use std::{env, error::Error};
 
 fn greet(req: &HttpRequest) -> impl Responder {
     let to = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", to)
 }
 
-fn main() {
+fn main() -> Result<(), Box<Error>> {
+    dotenv::dotenv().ok();
+
     env::set_var("RUST_LOG", "actix=info");
     env_logger::init();
 
+    let database_url = env::var("DATABASE_URL")?;
+
+    let _ = PgConnection::establish(&database_url)?;
+    println!("CONNECTED");
+
+    let host = if env::var("LOCAL_SERVER").ok().is_some() {
+        "127.0.0.1"
+    } else {
+        "0.0.0.0"
+    };
     let port = env::var("PORT").unwrap_or("8080".to_owned());
-    let host = format!("0.0.0.0:{}", port);
+
+    let host = format!("{}:{}", host, port);
 
     server::new(|| {
         App::new()
@@ -23,4 +39,6 @@ fn main() {
     }).bind(host)
         .unwrap()
         .run();
+
+    Ok(())
 }
